c581e32aea118ea7c1ae151248228053a918b311
noxter <mirko.dalfarra@gmail.com>
2022-05-29 22:15:49 +0200

    Webpack for handling style, typescript and version

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,65 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [
-                    path.resolve(__dirname, 'src')
+	context: path.join(__dirname, 'src'),
+	entry: {
+		index: path.join(__dirname, 'src', 'index.ts'),
+	},
+	output: {
+		filename: `[name].min.js`,
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist'
+	},
+	module: {
+		rules: [
+			{   test: /\.ts$/,
+                use: [
+                    'ts-loader'
+                ],
+                exclude: /node_modules/
+            },
+			{
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
                 ]
             }
-        ]
-    },
-    output: {
-        publicPath: 'dist',
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+		]
+	},
+	resolve: {
+		extensions: [
+            '.js',
+            '.jsx',
+            '.json',
+            '.scss',
+            '.css'
+        ],
+		modules: [
+            'node_modules'
+        ],
+		alias: {
+			'@root': path.join(__dirname, 'src'),
+		},
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ linkType: 'text/css', filename: `theme.css` }),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+                    from: 'version.json',
+                    to: 'version.json',
+                    force: true
+                },
+			],
+			options: {
+				concurrency: 100,
+			},
+		}),
+	],
     mode: 'development',
     devServer: {
         port: 8080,
