e00f73f9f904104100ab40ad295384594dfaae0e
noxter <mirko.dalfarra@gmail.com>
2022-06-29 21:21:47 +0200

    Better handling
    handling multiple styles per line, handling multiproperties styles. Fixing bugs

--- src/index.ts
+++ src/index.ts
@@ -58,21 +58,47 @@ const styleEnhancer = (_class: string) => {
 
 const classParser = (style: string) => {
     // Use a JSON to handle default module creation
+    let _stylesToApply: string = '' // This is what will be returned, as a set of styles all separated by ; and ' '
+    let styles: Array<string> | string | null = null
+
+    if (style.includes(' ')) {
+        styles = style.split(' ')
+    } else {
+        styles = style
+    }
+
+    if (Array.isArray(styles)) {
+        styles.forEach(style => {
+            _stylesToApply += classToStyle(style) + '; '
+        })
+    } else {
+        _stylesToApply = classToStyle(styles)
+    }
+    return _stylesToApply
+}
+
+const classToStyle = (_singleStyle: string) => {
     const moduleParser: object = PARSER
+    let parsed:any = moduleParser[_singleStyle.split('-')[0] as keyof object]
 
-    //const splittedStyle = style[1].split(/\d/)
-    //const measureUnit = splittedStyle[splittedStyle.length - 1]
 
-    let parsed:any = moduleParser[style.split('-')[0] as keyof object]
     if (parsed && parsed !== undefined) {
-        return parsed + ' ' +  parserRequest(style)
+        // style element has to contain both the value and the unit of measure for being a valid input
+        if (parsed.includes('!val') && parsed.includes('0!um')) {
+            const value = parserRequest(_singleStyle)
+            const um = reqUm(value)
+
+            return parsed.replace('!val', value).replace('!um', um)
+        }
+
+        return parsed + ' ' +  parserRequest(_singleStyle)
     } else {
         return ''
     }
 }
 
 const parserRequest = (_test: string) => {
-    const measureRegex: RegExp = /(\w+)-\[(\w*)?(0\.\w+)?\]$/g
+    const measureRegex: RegExp = /(\w+)-\[(\w*)?(0\.\w+(\%)?)?\]$/g
     const colorRegex: RegExp = /(\w+)-\[(\#(\d{6})?(a|b|c|d|e|f{6})?)?(rgb\((\d*){1,3},(\d*){1,3},(\d*){1,3}\))?\]$/g
     const exadecimalColorRegex: RegExp = /(\w+)-(\[(#(\w+))?\])$/g
     let _value: string = ''
@@ -89,7 +115,14 @@ const parserRequest = (_test: string) => {
             break;
     }
 
+    console.log('_value ' + _value)
     return _value
+}
+
+const reqUm = (_test: string) => {
+    // I split by number, this means that I split every number and use it to get the UM
+    let unit = _test.split(/(\d*((\.(\d*))?))/)[1]
+    return _test.split(unit)[1]
 }
 
 ApplyClassifying ()
